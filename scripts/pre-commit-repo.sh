fdd749319c35a8f1be0794c0f3230ce3a3c52d7e
Richard Kosegi <richard.kosegi@gmail.com>
2025-10-08 15:04:53 +0200

    ✨ Add pre-commit hooks definition
    Signed-off-by: Richard Kosegi <richard.kosegi@gmail.com>

---
+++ scripts/pre-commit-repo.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Copyright 2025 Richard Kosegi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+YP_BIN="${YP_BIN:-yp}"
+
+pipeline="pipeline.yaml"
+
+[ "${1:-}" = "--file" ] && ( shift; pipeline="${1}"; shift )
+
+${YP_BIN} --file "${pipeline}"
